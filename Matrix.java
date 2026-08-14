5535e356b64df47e53d1515fde7eea240bf1261b
yeshwanth-T-22 <yeshwanth-T-22@users.noreply.github.com>
2026-08-14 10:07:39 +0530

    Add Java programs

---
+++ Matrix.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Matrix {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int r1, c1, r2, c2;
+
+            System.out.print("Enter rows and columns of first matrix: ");
+            r1 = sc.nextInt();
+            c1 = sc.nextInt();
+
+            System.out.print("Enter rows and columns of second matrix: ");
+            r2 = sc.nextInt();
+            c2 = sc.nextInt();
+
+            if (c1 != r2) {
+                System.out.println("Matrix multiplication is not possible.");
+                return;
+            }
+
+            int[][] A = new int[r1][c1];
+            int[][] B = new int[r2][c2];
+            int[][] C = new int[r1][c2];
+
+            System.out.println("Enter elements of first matrix:");
+            for (int i = 0; i < r1; i++) {
+                for (int j = 0; j < c1; j++) {
+                    A[i][j] = sc.nextInt();
+                }
+            }
+
+            System.out.println("Enter elements of second matrix:");
+            for (int i = 0; i < r2; i++) {
+                for (int j = 0; j < c2; j++) {
+                    B[i][j] = sc.nextInt();
+                }
+            }
+
+            for (int i = 0; i < r1; i++) {
+                for (int j = 0; j < c2; j++) {
+                    for (int k = 0; k < c1; k++) {
+                        C[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+
+            System.out.println("Resultant Matrix:");
+            for (int i = 0; i < r1; i++) {
+                for (int j = 0; j < c2; j++) {
+                    System.out.print(C[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
